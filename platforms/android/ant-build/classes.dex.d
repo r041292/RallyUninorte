9a5a69350904ebdf6e2522dbd0854d3154640178
Luis Antonio <lmendoza.mg@gmail.com>
2014-05-10 17:34:18 -0500

    Nueva plataforma android, index google maps

---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,14 @@
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes.dex : \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes\com\phonegap\helloworld\BuildConfig.class \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes\com\phonegap\helloworld\HelloWorld.class \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes\com\phonegap\helloworld\R$attr.class \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes\com\phonegap\helloworld\R$drawable.class \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes\com\phonegap\helloworld\R$string.class \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes\com\phonegap\helloworld\R$xml.class \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes\com\phonegap\helloworld\R.class \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes\org\apache\cordova\geolocation\CordovaLocationListener$LocationTimeoutTask.class \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes\org\apache\cordova\geolocation\CordovaLocationListener.class \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes\org\apache\cordova\geolocation\GeoBroker.class \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes\org\apache\cordova\geolocation\GPSListener.class \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\classes\org\apache\cordova\geolocation\NetworkListener.class \
+C:\wamp\www\RallyUninorte\platforms\android\ant-build\dexedLibs\classes-327ffc051bc158287f71e908ea722047.jar \
