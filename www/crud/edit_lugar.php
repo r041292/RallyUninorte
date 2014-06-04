0bba9e93deeaa3d119bc849ff3df9efa9d188665
Demo <0041292@gmail.com>
2014-06-04 18:13:21 -0500

    remaining crud added

---
+++ www/crud/edit_lugar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="f/css/normalize.css">
+	<link rel="stylesheet" href="f/css/foundation.css">
+</head>
+<body>
+<? 
+include('config.php'); 
+if (isset($_GET['id_lugar']) ) { 
+$id_lugar = (int) $_GET['id_lugar']; 
+if (isset($_POST['submitted'])) { 
+foreach($_POST AS $key => $value) { $_POST[$key] = mysql_real_escape_string($value); } 
+$sql = "UPDATE `Lugar` SET  `nombre_lugar` =  '{$_POST['nombre_lugar']}' ,  `lat` =  '{$_POST['lat']}' ,  `long` =  '{$_POST['long']}' ,  `alt` =  '{$_POST['alt']}'   WHERE `id_lugar` = '$id_lugar' "; 
+mysql_query($sql) or die(mysql_error()); 
+echo (mysql_affected_rows()) ? "Edited row.<br />" : "Nothing changed. <br />"; 
+echo "<a href='list_lugar.php'>Back To Listing</a>"; 
+} 
+$row = mysql_fetch_array ( mysql_query("SELECT * FROM `Lugar` WHERE `id_lugar` = '$id_lugar' ")); 
+?>
+
+<form action='' method='POST'> 
+<p><b>Nombre Lugar:</b><br /><textarea name='nombre_lugar'><?= stripslashes($row['nombre_lugar']) ?></textarea> 
+<p><b>Lat:</b><br /><textarea name='lat'><?= stripslashes($row['lat']) ?></textarea> 
+<p><b>Long:</b><br /><textarea name='long'><?= stripslashes($row['long']) ?></textarea> 
+<p><b>Alt:</b><br /><textarea name='alt'><?= stripslashes($row['alt']) ?></textarea> 
+<p><input class='button' type='submit' value='Edit Row' /><input type='hidden' value='1' name='submitted' /> 
+</form> 
+<? } ?> 
+</body>
+</html>
---
+++ www/crud/edit_pista.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="f/css/normalize.css">
+	<link rel="stylesheet" href="f/css/foundation.css">
+</head>
+<body>
+<? 
+include('config.php'); 
+if (isset($_GET['id_pista']) ) { 
+$id_pista = (int) $_GET['id_pista']; 
+if (isset($_POST['submitted'])) { 
+foreach($_POST AS $key => $value) { $_POST[$key] = mysql_real_escape_string($value); } 
+$sql = "UPDATE `Pista` SET  `pista` =  '{$_POST['pista']}' ,  `id_lugar` =  '{$_POST['id_lugar']}'   WHERE `id_pista` = '$id_pista' "; 
+mysql_query($sql) or die(mysql_error()); 
+echo (mysql_affected_rows()) ? "Edited row.<br />" : "Nothing changed. <br />"; 
+echo "<a href='list_pista.php'>Back To Listing</a>"; 
+} 
+$row = mysql_fetch_array ( mysql_query("SELECT * FROM `Pista` WHERE `id_pista` = '$id_pista' ")); 
+?>
+
+<form action='' method='POST'> 
+<p><b>Pista:</b><br /><textarea name='pista'><?= stripslashes($row['pista']) ?></textarea> 
+<p><b>Id Lugar:</b><br /><input type='text' name='id_lugar' value='<?= stripslashes($row['id_lugar']) ?>' /> 
+<p><input class='button' type='submit' value='Edit Row' /><input type='hidden' value='1' name='submitted' /> 
+</form> 
+<? } ?> 
+</body>
+</html>
